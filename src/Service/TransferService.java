70d5820a730f80cbf6dcf77d2c2e2aab215ce2b6
igormartyniuk <igormartniuk@gmail.com>
2021-09-07 16:49:41 +0300

    0.02 add transfer
    transfer

--- src/Service/TransferService.java
+++ src/Service/TransferService.java
@@ -1,6 +1,12 @@
 package Service;
 
+import com.bank.lesson.entity.Account;
+
 public class TransferService {
+
+    public void transfer(Account fromAccount, Account toAccount, int amount){
+        System.out.println("перевод");
+    }
 
 
 }
